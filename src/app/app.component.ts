ef075b5d0f5224079dbb1cca2507cc7dfed759bd
Patil <Nilesh.Patil@emtecinc.com>
2017-10-04 18:47:42 +0530

    template driven approach implmented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm
+  genders = ['male', 'female'];
+  answer = '';
+  defaultQuestion = 'pet';
+  submitted = false;
+
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    answer: '',
+    gender: ''
+  }
+
+
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      username: suggestedName
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    this.submitted = true;
+    console.log(form);
+    this.user.username = form.value.username;
+    this.user.email = form.value.email;
+    this.user.secret = form.value.secret;
+    this.user.answer = form.value.questionAnswer;
+    this.user.gender = form.value.gender;
+    form.reset();
+    form.form.patchValue({
+      secret: this.defaultQuestion
+    });
   }
 }
